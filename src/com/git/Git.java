f7dc8b34148726eb6d24daf162d9e434be923d18
Abdul Azeez Khan <azeezkhan2197@gmail.com>
2020-03-07 15:28:44 +0530

    for pull

--- src/com/git/Git.java
+++ src/com/git/Git.java
@@ -3,8 +3,6 @@ package com.git;
 public class Git {
 	public static void main(String args[])
 	{
-		System.out.println("hii");
-		System.out.println("  world");
 
 		System.out.println(" hewllo world");
 	}
